d0c6c65a40849d75afb4768855a43c20d8c22feb
Amalec Harian Adrove Mejías <amalec.hack@gmail.com>
2014-08-01 19:49:55 -0400

    Video 2 : PHP & MySQL
    Formas de Imprimir datos/variables, uso de comillas, tipos de variables,
    variables array.

---
+++ Video_2_phpymysql-by-amalechack.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Videos de PHP y MySQL</title>
+</head>
+<body>
+<?php
+//Formas de imprimir
+/*
+echo "Hola";
+echo ' Mundo';
+print ",";
+print ' Cómo se encuentran ustedes en sus casas?';
+printf(", Yo me encuentro muy bien.");
+printf(' :)');*/
+
+//Diferencias de comillas
+
+/*$frase = "Hola Mundo!";
+print 'La frase es: '.$frase;
+#Entre comillas dobles se imprime el valor de la variable
+#Entre comillas simples se imprime el Nombre de la variable
+
+*/
+
+//Array = Tipos de variables que almacenan variables
+/*
+$variable_array = array(
+	'nombre_de_variable' => 10, #variable numérica (INT = integer)
+	'otra_variable' => "Texto", #variable texto (string = cadena) 
+	'variable_boolean' => true #variables boolean (true/false = 1/0)
+);
+
+print $variable_array['nombre_de_variable']; #esto nos devuelve el valor= 10
+*/
+?>	
+</body>
+</html>
